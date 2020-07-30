71e52ee25e6d53928d66e0144a82b18f5dd903d2
Sergio Eduardo Velandia <55334171+svelandi@users.noreply.github.com>
2020-07-30 11:13:44 -0500

    Plantilla Etiquetas

---
+++ m_plantillaEtiquetas.php
@@ -0,0 +1,369 @@
+<!-- Modal para Imprmir Etiquetas -->
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Etiquetas Virtuales</title>
+
+  <!-- Custom CSS -->
+  <link href="../../html/css/Imprimirpdf.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!-- <div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content"> -->
+
+      <!-- Modal Header -->
+      <!-- <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div> -->
+
+      <!-- Modal body -->
+      <!-- <div class="modal-body"> -->
+
+
+  <div>
+    <!-- <button type="button" class="close" data-dismiss="modal">&times;</button> -->
+    <h4 class="modal-title"> <b> ETIQUETAS VIRTUALES</b></h4>
+  </div>
+  <div class="contenedorEtiquetas">
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso1" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia1" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha1" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso1" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion1" class="form-control">
+      </div>
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden2" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso2" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia2" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha2" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso2" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion2" class="form-control">
+
+      </div>
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden3" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso3" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia3" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha3" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso3" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion3" class="form-control">
+
+      </div>
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden4" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso4" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia4" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha4" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso4" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion4" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden5" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso5" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia5" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha5" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso5" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion5" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden6" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso6" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia6" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha6" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso6" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion6" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden7" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso7" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia7" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha7" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso7" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion7" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden8" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso8" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia8" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha8" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso8" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion8" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden9" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso9" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia9" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha9 class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso9" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion9" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden10" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso10" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia10" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha10" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso10" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion10" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden11" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso11" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia11" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha11" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso11" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion11" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden12" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso12" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia12" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha12" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso12" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion12" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden13" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso13" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia13" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha13" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso13" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion13" class="form-control">
+
+      </div>
+      
+    </div>
+    <div class="margen">
+      <div class="etiquetaVirtual">
+        <label for="">ORDEN PROD:</label>
+        <input type="text" id="Orden14" class="form-control">
+
+        <label for="">PESO:</label>
+        <input type="text" id="peso14" class="form-control">
+
+        <label for="">REFERENCIA:</label>
+        <input type="text" id="Referencia14" class="form-control">
+
+        <label for="">Fecha:</label>
+        <input type="text" id="fecha14" class="form-control">
+
+        <label for="">DISPENSO:</label>
+        <input type="text" id="dispenso14" class="form-control">
+
+        <Label>VoBo QC:</Label>
+        <input type="text" id="aprobacion14" class="form-control">
+
+      </div>
+      
+    </div>
+  </div>
+</body>
+
+</html>
+
+<!-- </div> -->
+
+      <!-- Modal footer -->
+      <!-- <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div> -->
