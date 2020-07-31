7e7239e55f6fae9fd01721955e9abdb969a3c193
Esteban Gomez <josees_84@hotmail.com>
2020-07-31 08:54:07 -0500

    PruebaCompletada

--- Html/m_plantillaEtiquetas.php
+++ Html/m_plantillaEtiquetas.php
@@ -9,16 +9,15 @@
   <title>Etiquetas Virtuales</title>
 
   <!-- Custom CSS -->
-  <link href="Imprimirpdf.css" rel="stylesheet">
-  <script src="../JavaScript/datos.js">
-
-  </script>
+  <link href="Imprimirpdf.css" rel="stylesheet" media="all">
+  <script src="../JavaScript/datos.js"></script>
 
 </head>
 
 <body>
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
   <div>
     <h4 class="modal-title"> <b> ETIQUETAS VIRTUALES</b></h4>
   </div>
@@ -30,33 +29,37 @@
 
     </script>
   </div>
-  <!--
+
+
+
+  <!--<script>
+
+    function print()
+      {
+      var doc = new jsPDF();
+      doc.fromHTML($("#ContenedorP").html(),15,15)
+        "width":170,
+        "elementHandlers":specialElementHandlers
+
+      doc.save("sample-file.pdf");
+        }
+      }
+
+</script>-->
+
+
+
+  <button type="button"  id="imprime" name="button">Imprimir</button>
   <script>
-  function printDiv()
-  {
-  var divToPrint=document.getElementById('ContenedorP');
-  var newWin=window.open('','Print-Window');
-  newWin.document.open();
-  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
-  newWin.document.close();
-  setTimeout(function(){newWin.close();},10);
-  }
-</script>
--->
-</script>
-  <!--<button type="button" name="button" onclick="window.print()">Imprimir</button>-->
+      $(function(){
+        $('#imprime').click(function(){
+            printDiv();
+        });
+      });
+  </script>
+</div>
   <button type="button" name="button" onclick="window.print()">Imprimir</button>
+
 </body>
 
 </html>
-
-<!-- </div> -->
-
-      <!-- Modal footer -->
-      <!-- <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-      </div>
-
-    </div>
-  </div>
-</div> -->
--- JavaScript/datos.js
+++ JavaScript/datos.js
@@ -26,14 +26,15 @@ function cargar(){
           $('#ContenedorP').html(etiquetas);
      }
   })
+}
 
-  function printDiv()
-  {
-  var divToPrint=document.getElementById('ContenedorP');
-  var newWin=window.open('','Print-Window');
-  newWin.document.open();
-  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
-  newWin.document.close();
-  setTimeout(function(){newWin.close();},10);
-  }
+function printDiv()
+{
+    var divToPrint=document.getElementById('ContenedorP');
+    var newWin=window.open('','Print-Window');
+    newWin.document.open();
+    newWin.document.write('<html><body onload="window.print()">'+'<head><link href="Imprimirpdf.css" rel="stylesheet" media="all"></head>'+divToPrint.innerHTML+'</body></html>');
+    newWin.document.close();
+    setTimeout(function(){newWin.close();},10);
+
 }
